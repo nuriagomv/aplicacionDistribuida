c7a5b2c8b7ada92c2fc02379d74d8da50efd7f57
nuriagomv <nuriagomv@gmail.com>
2020-04-09 21:13:34 +0200

    esto a mi me funciona

--- servidorAhorcado.py
+++ servidorAhorcado.py
@@ -5,7 +5,81 @@ import time
 
 
 
-def pedirPalabras(conn):
+listaMonigotes = ['''
+ 
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+ =========''', '''
+ 
+   +---+
+   |   |
+   O   |
+       |
+       |
+       |
+ =========''', '''
+ 
+   +---+
+   |   |
+   O   |
+   |   |
+       |
+       |
+ =========''', '''
+ 
+   +---+
+   |   |
+   O   |
+  /|   |
+       |
+       |
+ =========''', '''
+ 
+   +---+
+   |   |
+   O   |
+  /|\  |
+       |
+       |
+ =========''', '''
+ 
+   +---+
+   |   |
+   O   |
+  /|\  |
+  /    |
+       |
+ =========''', '''
+ 
+   +---+
+   |   |
+   O   |
+  /|\  |
+  / \  |
+       |
+ =========''']
+
+nTotalIntentos = len(listaMonigotes)
+
+def mostrarTablero(listaMonigotes, letrasIncorrectas, letrasCorrectas, palabraSecreta):
+    print(listaMonigotes[len(letrasIncorrectas)-1])
+    print()
+ 
+    print('Letras incorrectas: ', letrasIncorrectas,'\n')
+
+    espaciosVacios = '_' * len(palabraSecreta)
+    for i in range(len(palabraSecreta)): # completar los espacios vacíos con las letras adivinadas
+        if palabraSecreta[i] in letrasCorrectas:
+            espaciosVacios = espaciosVacios[:i] + palabraSecreta[i] + espaciosVacios[i+1:]
+
+    print('Lo que llevas de la palabra: ',espaciosVacios, '\n')
+
+
+def pedirPalabras(conn): #también se puede aprovechar para pedir letras
     try:
         palabra = conn.recv()
     except EOFError:
@@ -56,6 +130,7 @@ def serve_client(jugador, ipPuerto, jugadores, cerrojo):
         if len(jugadores) == 2:
             cerrojo.acquire()
             pareja = jugadores.copy()
+            pareja['haGanado'] = [(1,'no'), (2,'no')]
             jugadores.clear()
             cerrojo.release()
             break
@@ -65,12 +140,22 @@ def serve_client(jugador, ipPuerto, jugadores, cerrojo):
     palabra = pedirPalabras(jugador)
 
     colocarPalabra(pareja, palabra, ipPuerto)
+
     print(pareja) #borrar cuando ya compruebe que el diccionario está bien
 
 
     #ahora aqui ya debe empezar el bucle del ahorcado
+    #el bucle (juego) terminará cuando el nIntentos==nTotalIntentos o cuando el otro jugador gane
+    #LISTA MONIGOTES Y NTOTALINTENTOS DEBERIA METERLOS DENTRO DE SERVER_CLIENT
     juegoContinua = True
+    letrasCorrectas = []
+    letrasIncorrectas = []
+    nIntentos = 0
     while juegoContinua:
+        juegoContinua = not ( nIntentos==nTotalIntentos or any([x=='ganador' for (_,x) in pareja['haGanado']]) )
+
+        #ACTUALIZAR LETRASCORRECTAS LETRASINCORRECTAS SEGUN JUGADOR Y PALABRACONTRARIA (CREO QUE HAY FUNCION)
+        #AÑADIR AQUI MOSTRAR TABLERO PARA JUGADOR Y PALABRA CONTRARIA
         try:
             m = jugador.recv()
         except EOFError:
